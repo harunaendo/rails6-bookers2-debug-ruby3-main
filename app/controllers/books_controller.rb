6c3a87d41385d9eea40ae9663c3c36e170ac5f23
haruna endo <163505612+harunaendo@users.noreply.github.com>
2024-07-03 05:27:35 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @user = User.find(current_user.id)
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def edit
+    is_matching_login_user
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    is_matching_login_user
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      @books = Book.all
+      render 'edit', books:@books
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+  book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+end
