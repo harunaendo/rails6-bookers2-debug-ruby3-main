da414f2913bbe3896f0354fd6136c3857c44aa30
EC2 Default User <ec2-user@ip-172-31-19-160.ap-northeast-1.compute.internal>
2024-07-09 07:35:54 +0000

    finish-5

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,18 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
   
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
   def follow(user) #get_profile_imageの下にかく
     #active_relationships.create(followed_id: user.id)
     relationships.create(followed_id: user.id)
