7288a27437fbc8317d2b3568487f8004abf16bae
EC2 Default User <ec2-user@ip-172-31-19-160.ap-northeast-1.compute.internal>
2024-07-09 05:57:29 +0000

    start-5

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,7 +10,7 @@ before_action :authenticate_user!
   def destroy
     user = User.find(params[:user_id])
     current_user.unfollow(user)
-    redirect_to  request.referer
+    redirect_to request.referer
   end
   
   def followings
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,29 +7,36 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  #has_many :followers, through: :passive_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  #has_many :followings, through: :active_relationships, source: :followed
+
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
   
-  def follow(user)
-    active_relationships.create(followed_id: user.id)
+  def follow(user) #get_profile_imageの下にかく
+    #active_relationships.create(followed_id: user.id)
+    relationships.create(followed_id: user.id)
   end
 
   def unfollow(user)
-    active_relationships.find_by(followed_id: user.id).destroy
+    #active_relationships.find_by(followed_id: user.id).destroy
+    relationships.find_by(followed_id: user.id).destroy
   end
   
   def following?(user) #ユーザーがフォローしているか判定
     followings.include?(user)
-  end
-  
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 end
